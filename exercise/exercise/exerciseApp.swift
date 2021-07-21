d485419bfbf98ca256c5b905295392917472372d
Jiaying Cheng <chengjiaying.prc@gmail.com>
2021-07-21 12:55:12 +0200

    basic init

---
+++ exercise/exercise/exerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  exerciseApp.swift
+//  exercise
+//
+//  Created by Codecamp on 21.07.21.
+//
+
+import SwiftUI
+
+@main
+struct exerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
